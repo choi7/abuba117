411b9a1e676ecf665b427528885da0cd336fc0b0
hyoshinchoi <sshin369@naver.com>
2018-01-17 15:58:33 +0900

    ss

---
+++ app/src/main/java/com/joyblock/abuba/api_message/R18_insertSurvey.java
@@ -0,0 +1,9 @@
+package com.joyblock.abuba.api_message;
+
+/**
+ * Created by hyoshinchoi on 2018. 1. 17..
+ */
+
+public class R18_InsertSurvey {
+    public String seq_user,seq_kindergarden,seq_kindergarden_class,title, content, year, month, day, c_survey_vote, files, vote_item;
+}
