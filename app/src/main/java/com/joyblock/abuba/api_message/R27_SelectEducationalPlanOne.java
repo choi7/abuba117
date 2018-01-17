d9b9a4bb8247938151d8fe758103fcf0ab2a6aad
POPCON <jnsoo.pp@gmail.com>
2018-01-17 17:50:21 +0900

    박준수 수정

---
+++ app/src/main/java/com/joyblock/abuba/api_message/R27_SelectEducationalPlanOne.java
@@ -0,0 +1,9 @@
+package com.joyblock.abuba.api_message;
+
+/**
+ * Created by POPCON on 2018-01-16.
+ */
+
+public class R27_SelectEducationalPlanOne {
+    public String seq_kindergarden_class,file_path,reg_date,seq_kindergarden,is_yn,plan_flag,mod_date,seq_user,seq_educational_plan,title,content;
+}
