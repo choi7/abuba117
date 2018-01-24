815a0cbf12b0fed7e3cfb9bb627263a290a4a857
최효신 <sshin369@naver.com>
2018-01-24 19:42:11 +0900

    효신업

--- app/src/main/java/com/joyblock/abuba/document/A3_1_Medicine_Parent.java
+++ app/src/main/java/com/joyblock/abuba/document/A3_1_Medicine_Parent.java
@@ -6,9 +6,11 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,13 +19,24 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.GsonBuilder;
 import com.joyblock.abuba.BaseActivity;
 import com.joyblock.abuba.R;
+import com.joyblock.abuba.api_message.R17_SelectMyKidsList;
+import com.joyblock.abuba.api_message.R34_SelectMedicationRequestList;
 import com.joyblock.abuba.notice.BanListViewAdapter;
 import com.joyblock.abuba.notice.BanListViewItem;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 
 public class A3_1_Medicine_Parent extends BaseActivity {
 
@@ -199,6 +212,77 @@ public class A3_1_Medicine_Parent extends BaseActivity {
 
 
     }
+
+
+    R34_SelectMedicationRequestList[] r34_selectMedicationRequestLists;
+    class SelectMedicationRequestList extends AsyncTask<Void, Void, String> {
+        OkHttpClient client;
+        okhttp3.Request request;
+        RequestBody formBody;
+
+        public SelectMedicationRequestList(String seq_kindergarden, String seq_kindergarden_class, String year, String month, String day) {
+            client = new OkHttpClient();
+            formBody = new FormBody.Builder()
+                    .add("seq_kindergarden", seq_kindergarden)
+                    .add("seq_kindergarden_class", seq_kindergarden_class)
+                    .add("year", year)
+                    .add("month", month)
+                    .add("day", day)
+                    .build();
+            request = new okhttp3.Request.Builder()
+                    .url("http://58.229.208.246/Ububa/selectMyKidsList.do")
+                    .post(formBody)
+                    .build();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                okhttp3.Response response = client.newCall(request).execute();
+                if (!response.isSuccessful()) {
+                    return null;
+                }
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            super.onPostExecute(json);
+            Log.d("TAG", json);
+            try {
+                JSONObject jsonResponse = new JSONObject(json);
+                System.out.println(jsonResponse);
+                Integer ss = Integer.parseInt(jsonResponse.getString("resultCode"));
+                System.out.println(ss);
+
+                if (ss == 200) {
+                    String resultCode = jsonResponse.getString("resultCode");
+                    String resultMsg = jsonResponse.getString("resultMsg");
+                    Log.d("resultCode : ", resultCode);
+                    Log.d("resultMsg : ", resultMsg);
+
+                    r34_selectMedicationRequestLists=new GsonBuilder().create().fromJson(jsonResponse.getString("my_kids_list"),R34_SelectMedicationRequestList[].class);
+                    Log.d("detail" , String.valueOf(r34_selectMedicationRequestLists));
+                    Log.d("details" , String.valueOf(r34_selectMedicationRequestLists.length));
+                    JSONArray json3 = new JSONArray(jsonResponse.getString("my_kids_list"));
+                    System.out.print("sdf");
+                    System.out.print(json3);
+                    System.out.print("sdf");
+                    JSONObject json4 = new JSONObject(json3.getString(0));
+                    Log.d("json4", String.valueOf(json4));
+                } else if (ss == 102) {
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 
 
 }
