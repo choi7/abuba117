7410cddef2973306f2d13a6ef4016da36a311d40
pjnsoo <jnsoo.pp@gmail.com>
2018-01-25 20:13:48 +0900

    박준수 수정

--- app/src/main/java/com/joyblock/abuba/test_park/A2_1_boading_management.java
+++ app/src/main/java/com/joyblock/abuba/test_park/A2_1_boading_management.java
@@ -1,51 +1,120 @@
 package com.joyblock.abuba.test_park;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.joyblock.abuba.BaseActivity;
+import com.joyblock.abuba.CustomListViewDialog;
 import com.joyblock.abuba.R;
+import com.joyblock.abuba.api_message.R6_SelectKindergardenClassList;
+import com.joyblock.abuba.notice.BanListViewAdapter;
+import com.joyblock.abuba.notice.BanListViewItem;
+//import com.joyblock.abuba.api_message.R6_SelectKindergardenClassList;
 
 public class A2_1_boading_management extends BaseActivity{
 
 
-    String boarding_str;
+    String str_boarding;
     SharedPreferences.Editor editor;
     boolean flag;
 
-    BoardingListViewAdapter adapter;
-    ListView listView;
 
+    int int_selected_class;
+
+
+    CustomListViewDialog dialog;
+
+//    AlertDialog.Builder classListDialogBuilder;//,modDelDialogBuidler;
+//    DialogInterface classListDialogInterface;//,modeDelDialogInteface;
+
+    ConstraintLayout class_ConstraintLayout;
+
+    TextView classText;
+
+    BoardingListViewAdapter adapter;
+    BanListViewAdapter class_adapter;
+    ListView listview,class_listview;
+    Activity activity;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activity=this;
         flag=true;// 승/하차 플래그
         setContentView(R.layout.layout_a2_1_boading_management);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xff9966ff));
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
 
 
-        editor=pref.edit();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        String seq_user = pref.getString("seq_user","없음");
-        String seq_kindergarden = pref.getString("seq_kindergarden","없음");
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setStatusBarColor(Color.parseColor("#9966FF"));
+        }
+        if (Build.VERSION.SDK_INT >= 23) {
+            getWindow().setStatusBarColor(Color.parseColor("#FFFFFF"));
+        }
+
+//        editor=pref.edit();
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        String seq_user = app.seq_user;
+        String seq_kindergarden = app.my_kindergarden_list.get(0).seq_kindergarden;
         String notice_title="긴급공지";
         String content="내용없음";
         String files="no files";
 
+        classText = (TextView) findViewById(R.id.a3_1_Class_TextView);
 
 
 
         actionbarCustom();
 
-        boarding_str="m";
-        callFragment(boarding_str);
+        str_boarding="m";
+        callFragment(str_boarding);
+
+
+        class_ConstraintLayout = (ConstraintLayout) findViewById(R.id.a3_1ClassConstraintLayout);
+
+        //반 검색 부부 클릭 다이얼로그 생성 이벤트
+        class_ConstraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //어댑터 생성 후 customListViewDialog에 반 리스트를 보여준다.
+                class_adapter = new BanListViewAdapter();
+                dialog = new CustomListViewDialog(A2_1_boading_management.this,class_adapter);
+                class_adapter.addItem("전체");
+                for (R6_SelectKindergardenClassList list : app.kindergarden_class_list) {
+                    class_adapter.addItem(list.kindergarden_class_name);
+                }
+                class_adapter.notifyDataSetChanged();
+                dialog.show();
+
+                //반 다이얼로그 이벤트 처리
+                class_listview=dialog.getListView();
+                class_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        BanListViewItem item = class_adapter.list.get(position);
+                        int_selected_class=position;
+                        //seq_kindergarden_class = position == 0 ? "0" : classList[position - 1].seq_kindergarden_class;
+                        classText.setText(item.getName());
+                        Toast.makeText(getApplicationContext(), position == 0 ? "전체" : item.getName(), Toast.LENGTH_LONG).show();
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
 
 
 
@@ -58,7 +127,7 @@ public class A2_1_boading_management extends BaseActivity{
         // 프래그먼트 사용을 위해
         android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         A2_1_boarding_management_fragment fragment = new A2_1_boarding_management_fragment();
-        fragment.setFlag(boarding_str);
+        fragment.setFlag(str_boarding);
         fragment.setPref(pref);
         transaction.replace(R.id.education_plan_fragment_container, fragment);
         transaction.commit();
@@ -125,13 +194,13 @@ public class A2_1_boading_management extends BaseActivity{
 
 
     public void clickLeft(View v){
-        boarding_str="m";
-        callFragment(boarding_str);
+        str_boarding="m";
+        callFragment(str_boarding);
     }
 
     public void clickRight(View v){
-        boarding_str="w";
-        callFragment(boarding_str);
+        str_boarding="w";
+        callFragment(str_boarding);
     }
 
 
