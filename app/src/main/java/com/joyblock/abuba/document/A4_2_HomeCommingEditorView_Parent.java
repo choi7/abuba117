b3ec7851aa957c8d7ff5f0272caee976ae934530
pjnsoo <jnsoo.pp@gmail.com>
2018-01-26 16:32:53 +0900

    박준수 수정

--- app/src/main/java/com/joyblock/abuba/document/A4_2_HomeCommingEditorView_Parent.java
+++ app/src/main/java/com/joyblock/abuba/document/A4_2_HomeCommingEditorView_Parent.java
@@ -196,7 +196,7 @@ public class A4_2_HomeCommingEditorView_Parent extends BaseActivity {
         checkImage3 = (ImageView) findViewById(R.id.a3_3_temp3ImageView);
         medicinePhotoImageView = (ImageView) findViewById(R.id.medicinePhotoImageView);
 
-        pushText = (TextView) findViewById(R.id.medicineViewPushText);
+        pushText = (TextView) findViewById(R.id.textview_ok);
         timeText = (TextView) findViewById(R.id.a3_2_timeText);
         medicineViewPushText = (TextView) findViewById(R.id.medicineViewPushText);
 
